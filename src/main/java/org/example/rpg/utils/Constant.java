4ae1ffe0b65fe708c4d463a904f4cb276c7ecf72
alisamettan <alisamet_tan@hotmail.com>
2024-04-03 15:20:59 +0200

    Proje tamamlandı.

---
+++ src/main/java/org/example/rpg/utils/Constant.java
@@ -0,0 +1,10 @@
+package org.example.rpg.utils;
+
+public class Constant {
+
+    private Constant(){//Constructor private oluşturuldu bundan sonra obje oluşturalamaz.
+
+    }
+    public  static final double POISON_PERCENTAGE=0.3;
+    public static final double BLEED_PERCENTAGE=0.25;
+}
